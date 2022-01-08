4f9f1abd37efd61cf47477a50cde8fad56717584
andresMucino <mucino.andres1990@gmail.com>
2022-01-08 15:15:22 -0600

    mongo

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { UsersModule } from 'src/users/users.module';
+import { AuthController } from './controllers/auth.controller';
+import { AuthService } from './service/auth.service';
+import { LocalStrategy } from './strategies/local.strategy';
+
+@Module({
+  imports: [UsersModule, PassportModule],
+  providers: [AuthService, LocalStrategy],
+  controllers: [AuthController],
+})
+export class AuthModule {}
---
+++ src/users/services/users.service.ts
@@ -0,0 +1,65 @@
+import { Injectable, Inject } from '@nestjs/common';
+import { Db } from 'mongodb';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import * as bcrypt from 'bcrypt';
+
+import { User } from '../entities/user.entity';
+import { CreateUserDto, UpdateUserDto } from '../dtos/user.dto';
+import { ProductsService } from '../../products/services/products.service';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    private productsService: ProductsService,
+    @Inject('MONGO') private databaseMongo: Db,
+    @InjectModel(User.name) private userModel: Model<User>,
+  ) {}
+
+  findAll() {
+    return this.userModel.find().exec();
+  }
+
+  getTasks() {
+    const tasksCollection = this.databaseMongo.collection('tasks');
+    return tasksCollection.find().toArray();
+  }
+
+  async findOne(id: string) {
+    return this.userModel.findById(id);
+  }
+
+  async getOrdersByUser(userId: string) {
+    const user = await this.findOne(userId);
+    return {
+      date: new Date(),
+      user,
+      // products: this.productsService.findAll(),
+      products: [],
+    };
+  }
+
+  findByEmail(email: string) {
+    return this.userModel.findOne({ email }).exec();
+  }
+
+  async create(data: CreateUserDto) {
+    const newModel = new this.userModel(data);
+    const hashPassword = await bcrypt.hash(newModel.password, 10);
+    newModel.password = hashPassword;
+    const model = await newModel.save();
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { password, ...rta } = model.toJSON();
+    return rta;
+  }
+
+  update(id: string, changes: UpdateUserDto) {
+    return this.userModel
+      .findByIdAndUpdate(id, { $set: changes }, { new: true })
+      .exec();
+  }
+
+  remove(id: string) {
+    return this.userModel.findByIdAndDelete(id);
+  }
+}
